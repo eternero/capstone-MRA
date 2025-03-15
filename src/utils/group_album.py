12e4011c5f4a60f3415534343988915deec72621
AngelGonzs <angel.gonzalez49@upr.edu>
2025-03-14 21:35:22 -0400

    deleted file... not sure if it was even tracked.

--- src/utils/group_album.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/Users/nico/Desktop/CIIC/CAPSTONE/essentia_demo/metadata.csv')
-
-# Group by 'ALBUM' and 'ARTIST' by dropping duplicates
-grouped_df = df[['ALBUM', 'ARTIST']].drop_duplicates()
-
-# Save the grouped DataFrame to a new CSV file
-grouped_df.to_csv('grouped_output.csv', index=False)
