8cb98574e5a6f4f72b03e62296ec8f68c5055d75
AngelGonzs <angel.gonzalez49@upr.edu>
2025-03-16 06:46:02 -0400

    Work in Progress : Refactoring Nodriver code for scraping. Mostly done.

--- src/scrape/nodriver_testing.py
+++ src/scrape/nodriver_testing.py
@@ -1,5 +1,6 @@
 """..."""
 import os
+import csv
 import json
 import random
 import asyncio
@@ -7,7 +8,7 @@ import logging
 import nodriver
 import pandas as pd
 from nodriver import Tab, Browser
-from src.utils.clean_csv import get_missing_albums
+from src.utils.clean_csv import get_query_columns
 
 # Configuring the logging
 logging.basicConfig(
@@ -17,216 +18,222 @@ logging.basicConfig(
     filemode='a'
 )
 
-PROGRESS_FILE = "progress.json"
-RESULT_FILE = "rym_result.json"
+PROGRESS_FILE = "progress.csv"
 
+class NoDriving:
+    """Object whose purpose is to scrape data in an undetected manner from websites. For us, our
+    target is RateYourMusic (RYM) - from which we will be getting User-Generated Content (UGC) such
+    as Primary Genre, Secondary Genre and Album Descriptors.
 
-async def save_progress(last_index: int, album_cache: dict):
-    """
-    Saves the last successful track index to a file. This is useful in case of any interruptions
-    or errors during runtime, allowing us to know when was the progress cut off.
-    """
-    progress_data = {
-        "last_index": last_index,
-        "album_cache": album_cache
-    }
+    This object is concerned with only two things really
+        1. Scraping the data from RateYourMusic.
+        2. Saving and Loading that very same data.
 
-    with open(PROGRESS_FILE, "w") as f:
-        json.dump(progress_data, f)
+    Attributes:
+        dataset_path  : The path to the csv file that contains the albums to be searched for. It
+                        must only have the columns `ARTIST` and `ALBUM`. It does not need to be
+                        cleaned either, this class will take care of doing so.
+        progress_path : The path pointing to the csv file in which we will save the RYM data. If one
+                        does not exist, it will be created.
+        album_df      : The DataFrame which contains all of our relevant album data.
 
-
-async def load_progress() -> tuple[int, dict]:
-    """
-    Loads the last successful track index from a file.
-    """
-    if os.path.exists(PROGRESS_FILE):
-        with open(PROGRESS_FILE, "r") as f:
-            data = json.load(f)
-            last_index = data.get("last_index", 0)
-            album_cache = data.get("album_cache", {})
-            return last_index, album_cache
-    return 0, {}
-
-
-async def check_for_error(page: Tab) -> bool:
-    """Checks if we've got an error when searching for our page."""
-    try:
-        # Attempt to select the element with a 1.5s timeout.
-        await page.select("div[class=page_error_content]", timeout=random.uniform(3.5,5))
-        return True    # There is an error, true!
-
-    except Exception:
-        # If any exception occurs, assume the element is not present
-        return False    # There are no errors, false!
-
-
-async def random_interactions(page: Tab):
-    """
-    This method attempts to mimic human interactions when accessing a page. We're mainly
-    concerned with creating delays and scrolling through the page while data is retrieved.
+    DataFrames:
+        album_df      : The main DataFrame to be used. It is extracted from `dataset_path` and then
+                        cleaned and normalized - we also compare it to whatever we've gone over in
+                        the `progress_path` to avoid repetition. This will be used to iterate over
+                        the albums for which we are yet to acquire their data.
+        progress_df   : The DataFrame used to represent our previously acquired data. This is only
+                        briefly used, since its only utility is to allow us to intuitively compare
+                        it with the `album_df` to figure out what albums remain to be retrieved.
     """
 
-    # Scroll three times with random directions and distances
-    for _ in range(random.randint(1,5)):
-        choice     = random.choice([True, False])
-        scroll_val = round(random.uniform(50, 200), 3)
+    def __init__(self, dataset_path : str, progress_path : str):
+        self.dataset_path  = dataset_path
+        self.progress_path = progress_path
 
-        # Scroll up or down depending on the random values previously acquired.
-        scroll     = page.scroll_down if choice else page.scroll_up
-        await scroll(scroll_val)
+        # Define constants.
+        self.attributes    = [
+                                "artist", "album",
+                                "pri_genres", "sec_genres", "pri_desc"
+                             ]
 
-        # Introduce a short delay between scrolls to mimic reading time
-        await asyncio.sleep(random.uniform(0.5, 2))
+        self.span_classes  = {
+                                'pri_genres' : 'release_pri_genres',
+                                'sec_genres' : 'release_sec_genres',
+                                'pri_desc'   : 'release_pri_descriptors'
+                             }
 
+        self.release_types = [
+                                'album', 'ep', 'mixtape', 'comp'
+                             ]
 
-async def retrieve_rym_data(browser: Browser, artist_name: str, album_name: str) -> dict[str]:
-    """
-    ...
-    """
+        # Initialize our dataframes.
+        progress_exists    = os.path.exists(progress_path) and not os.path.getsize(progress_path) == 0
+        self.progress_df   = (
+                                pd.read_csv(progress_path) if progress_exists
+                                else pd.DataFrame(columns=self.attributes)
+                             )
 
-    # Define constants and other vars.
-    results         =  {}
-    span_class_dict =  {
-                        'pri_genres' : 'release_pri_genres',
-                        'sec_genres' : 'release_sec_genres',
-                        'pri_desc'   : 'release_pri_descriptors'
-                        }
-    release_type_list = ['album', 'ep', 'mixtape', 'comp']
-
-    for release_type in release_type_list:
-
-        # Access the page
-        url   = f"https://rateyourmusic.com/release/{release_type}/{artist_name}/{album_name}/"
-        page  = await browser.get(url)
-        logging.info("Accessing URL: %s", url)
+        self.album_df      = self.get_missing_albums()
 
 
-        # Check for errors...
-        error = await check_for_error(page)
-        if error:
-            await asyncio.sleep(random.uniform(1.5,3))
-            continue                    # If there is an error, try the next release type
-        await random_interactions(page) # Otherwise, randomize interactions while acquiring data.
+    def save_progress(self, album_data : dict[str]):
+        """
+        This methods adds a new line to a CSV with data from RYM. If the CSV does not yet exist, it
+        will take the task of initializing it with the columns:
+            `artist, album, pri_genre, sec_genre, pri_desc`
+        """
+
+        # Open the file in append mode
+        with open(self.progress_path, mode = "a", newline = "", encoding = "utf-8") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames = self.attributes)
+
+            # If the file does not exist or is empty, write the header.
+            if not os.path.exists(self.progress_path) or os.path.getsize(self.progress_path) == 0:
+                writer.writeheader()
+
+            # Write the data into file.
+            writer.writerow(album_data)
 
 
-        # Commence gathering the data...
-        try:    # NOTE : Susceptible to CAPTCHA taking longer than 10s.
-            for span_name, span_class in span_class_dict.items():
-                element  = await page.select(f"span[class={span_class}]")
-                all_data = element.text_all
+    def get_missing_albums(self):
+        """
+        Compares the `album_df` against the data in the .csv located at `progress_path`. It will
+        update the album list to only include those albums whose data has not been retrieved yet.
 
-                logging.info("Successfully retreived data from RYM: %s", span_class)
-                results[span_name] = [data.strip().lower() for data in all_data.split(",")]
+        If there does not exist any .csv at `progress_path` yet, then it will simply return the
+        entirety of `album_df` unaltered.
+        """
+        album_df            = get_query_columns(self.dataset_path)
+        progress_pairs      = set(zip(self.progress_df["album"], self.progress_df["artist"]))
 
+        # Get the mask which will indicate all of the missing albums
+        remainder_mask      = [
+                                (row["query_album"], row["query_artist"]) not in progress_pairs
+                                for _, row in album_df.iterrows()
+                              ]
 
-        except Exception as e:
-            # If any span fails, we skip this release type altogether.
-            logging.error("Failed to retrieve data for release_type='%s'. Reason: %s",
-                          release_type, e)
-            continue
-
-        # We can only possibly get here if there were no exceptions
-        # when acquiring the data. Meaning the retrieval succeeded.
-        break
-
-    return results
+        return album_df[remainder_mask]
 
 
-async def retrieve_all_albums_data(album_list: list[str], recount: bool = False) -> list[dict[str]]:
-    """
-    ...
+    @staticmethod
+    async def check_for_error( page: Tab) -> bool:
+        """Checks if we've got an error when searching for our page."""
+        try:
+            # Attempt to select the element with a 1.5s timeout.
+            await  page.select("div[class=page_error_content]", timeout=random.uniform(3.5,5))
+            return True     # There is an error, true!
 
-    Args:
-        albums (dict[str, str]): _description_
+        except Exception:
+            # If any exception occurs, assume the element is not present
+            return False    # There are no errors, false!
 
-    Returns:
-        list[dict[str,str]]: _description_
-    """
-    results = []
-    browser = await nodriver.start()
 
-   # Load previous progress, if available.
-    start_index, album_cache = await load_progress()
-    album_request_counter    = 0
-    total_albums             = len(album_list)
-    start_index              = 0 if recount else start_index
+    @staticmethod
+    async def random_interactions(page: Tab):
+        """
+        This method attempts to mimic human interactions when accessing a page. We're mainly
+        concerned with creating delays and scrolling through the page while data is retrieved.
+        """
 
-    for i in range(start_index, total_albums):
-        album_attributes      = album_list[i]
-        artist_name           = album_attributes['query_artist']
-        album_name            = album_attributes['query_album']
+        # Scroll three times with random directions and distances
+        for _ in range(random.randint(2,6)):
+            choice     = random.choice([True, False])
+            scroll_val = round(random.uniform(50, 200), 3)
 
-        album_key             = f"{artist_name}/{album_name}"
-        album_request_counter += 1
+            # Scroll up or down depending on the random values previously acquired.
+            scroll     = page.scroll_down if choice else page.scroll_up
+            await scroll(scroll_val)
 
-        if album_key in album_cache:
-            logging.info("Using cached data for album: %s", album_key)
-            album_data = album_cache[album_key]
+            # Introduce a short delay between scrolls to mimic reading time
+            await asyncio.sleep(random.uniform(1, 2.5))
 
-        else:
-            album_data = await retrieve_rym_data(browser, artist_name, album_name)
+
+    async def retrieve_rym_data(self, browser: Browser,
+                                artist_name  : str,
+                                album_name   : str) -> dict[str]:
+        """
+        Retrives the data from RateYourMusic for one release (album, ep, mixtape or compilation).
+        """
+
+        results   =  {}
+        for release in self.release_types:
+
+            # Access the page.
+            url   = f"https://rateyourmusic.com/release/{release}/{artist_name}/{album_name}/"
+            page  = await browser.get(url)
+            logging.info("Accessing URL: %s", url)
+
+
+            # Check for errors.
+            error = await NoDriving.check_for_error(page)
+            if error:
+                await asyncio.sleep(random.uniform(2.5, 5))
+                continue                                # If there is an error, try the next release type
+            await NoDriving.random_interactions(page)   # Otherwise, randomize interactions while acquiring data.
+
+            # Commence gathering the data..
+            try:    # NOTE : Susceptible to CAPTCHA taking longer than 10s.
+                for span_name, span_class in self.span_classes.items():
+                    element            = await page.select(f"span[class={span_class}]")
+                    all_data           = element.text_all
+
+                    logging.info("Successfully retreived data from RYM: %s", span_class)
+                    results[span_name] = json.dumps([data.strip().lower()               # Not sure if
+                                                     for data in all_data.split(",")])  # this will work...
+
+
+            except Exception as e:
+                # If any span fails, we skip this release type altogether.
+                logging.error("Failed to retrieve data for release type ='%s'. Reason: %s",
+                            release, e)
+                continue
+
+            # We can only possibly get here if there were no exceptions
+            # when acquiring the data. Meaning the retrieval succeeded.
+            break
+
+        return results
+
+
+    async def retrieve_all_albums_data(self):
+        """
+        Iterates through the entirety of `album_df` and attempts to retrieve all of the data
+        from RateYourMusic for each of the albums in this DataFrame. As it proceeds, it will
+        continually save the progress.
+        """
+        # Start up the browser!
+        browser = await nodriver.start()
+
+        for row in self.album_df.itertuples(index=True):
+            artist_name = row.query_artist
+            album_name  = row.query_album
+
+            album_data  = await self.retrieve_rym_data(browser, artist_name, album_name)
             if not album_data:  # If no data was retrieved, there must've been failure. Thus, skip.
                 logging.warning("Could not find data for %s : %s", artist_name, album_name)
                 continue
 
-            album_cache[album_key] = album_data
+            # Save the data we've acquired
+            retrieved_data = {"artist" : artist_name, "album" : album_name}
+            retrieved_data.update(album_data)
 
-        # Save the data we've acquired
-        results.append({
-                        "artist_name": artist_name,
-                        "album_name" : album_name,
-                        "data"       : album_data
-                       })
-        await save_progress(i, album_cache)
+            # Write it to the csv file at `self.progress_path`.
+            self.save_progress(retrieved_data)
 
-        # Every 10 requests, take a break of 20-30 seconds.
-        if album_request_counter % 10 == 0:
-            long_break = random.uniform(20, 30)
-            logging.info("Taking a long break for %d seconds after %d request",
-                         long_break, album_request_counter)
-            await asyncio.sleep(long_break)
+            # Every 10 requests, take a break of 20-30 seconds.
+            if (row.Index + 1) % 10 == 0:
+                long_break = random.uniform(20, 30)
+                logging.info("Taking a long break for %d seconds after %d request",
+                             long_break, row.Index)
+                await asyncio.sleep(long_break)
 
-
-    # If we've managed to get all the data without any errors,
-    # write it in the `RESULT_FILE` to finish off.
-    with open(RESULT_FILE, "w") as f:
-        json.dump(results, f, indent=4)
-
-    logging.info("All album data successfully retrieved")
-    return results
 
 
 if __name__ == '__main__':
-    csv_path   = '/Users/nico/Desktop/CIIC/CAPSTONE/essentia_demo/grouped_output_750_v2_clean.csv'
-    json_path  = '/Users/nico/Desktop/CIIC/CAPSTONE/essentia_demo/progress.json'
-    track_list = get_missing_albums(csv_path=csv_path, progress_path=json_path)
+    CSV_PATH   = '/Users/nico/Desktop/CIIC/CAPSTONE/essentia_demo/grouped_output_750_v2_clean.csv'
+    no_driving = NoDriving(CSV_PATH, PROGRESS_FILE)
 
     logging.info("Starting scraping process")
-    nodriver.loop().run_until_complete(retrieve_all_albums_data(track_list, recount=True))
+    nodriver.loop().run_until_complete(no_driving.retrieve_all_albums_data())
     logging.info("Scraping process completed")
-
-
-# NOTE : Also missing albums from artists which were featured twice...
-
-
-"""MISSED :
-Could not find data for aap-rocky : testing
-Could not find data for aphex-twin : selected-ambient-works-8592
-Could not find data for arcángel : feliz-navidad
-Could not find data for asunojokei : island
-Could not find data for at-the-drivein : relationship-of-command
-Could not find data for beyoncé : lemonade
-Could not find data for bizarrap-milo-j : en-dormir-sin-madrid
-Could not find data for björk : post
-Could not find data for camilasin-bandera : 4-latidos-tour-en-vivo
-Could not find data for carlos-vives : corazón-profundo
-Could not find data for cursive : cursives-domestica
-Could not find data for dreamville-and-j-cole : revenge-of-the-dreamers-iii-directors-cut
-Could not find data for eladio-carrión : 3men2-kbrn
-Could not find data for evilgiane : evilslime
-Could not find data for freddie-gibbs : bandana
-Could not find data for harto-falión : im_my_best_friend
-Could not find data for hic : l3ft-4-d3ad
-Could not find data for jai-paul : leak-0413
-Could not find data for jay-wheeler : platonicos
-"""
--- src/utils/clean_csv.py
+++ src/utils/clean_csv.py
@@ -1,15 +1,9 @@
 """..."""
 import os
 import re
-import json
 import unicodedata
 import pandas as pd
 from pprint import pprint
-
-
-import re
-import unicodedata
-
 
 
 def process_name(name: str) -> str:
@@ -20,8 +14,17 @@ def process_name(name: str) -> str:
     # 1. Normalize the string using NFKC (this still composes characters but doesn't force ASCII)
     name = unicodedata.normalize('NFKC', name)
 
-    # 2. Replace dollar signs with the letter "s"
-    name = name.replace('$', 's')
+    # 1.5 Second round of normalization.
+    decomposed_name = unicodedata.normalize('NFD', name)
+    filtered_name   = []
+    for char in decomposed_name:
+        if unicodedata.combining(char):
+            continue
+        filtered_name.append(char)
+    name = unicodedata.normalize('NFC', "".join(filtered_name))
+
+    # 2. Replace dollar signs with underscore "_"
+    name = name.replace('$', '_')
 
     # 3. Remove anything inside parentheses or square brackets (including the brackets)
     name = re.sub(r'\(.*?\)', '', name)
@@ -51,7 +54,7 @@ def process_name(name: str) -> str:
     return name
 
 
-def get_query_columns(album_df : pd.DataFrame) -> list[str]:
+def get_query_columns(csv_path : str) -> pd.DataFrame:
     """Adds the query columns to our DataFrame. This method assumes that the column names
         'ALBUM' and 'ARTIST'
        are part of the dataframe... otherwise it will throw an error of course. It then just
@@ -63,32 +66,13 @@ def get_query_columns(album_df : pd.DataFrame) -> list[str]:
             'burial': 'untrue',
         }
     """
-    album_df['query_artist'] = album_df['ARTIST'].apply(process_name)
-    album_df['query_album']  = album_df['ALBUM'].apply(process_name)
-    return album_df[['query_artist', 'query_album']].to_dict(orient='records')
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError(f"No file found at '{csv_path}'")
 
-
-def get_missing_albums(csv_path : str, progress_path : str):
-    album_df    = pd.read_csv(csv_path)
-    album_list  = get_query_columns(album_df)
-    result_list = []
-
-    with open(progress_path, "r") as file:
-        data        = json.load(file)
-        album_cache = data.get("album_cache", {})
-
-    for album_data in album_list:
-        artist    = album_data['query_artist']
-        album     = album_data['query_album']
-        album_key = f"{artist}/{album}"
-
-        if album_key not in album_cache:
-            result_list.append({'query_artist' : artist, 'query_album' : album})
-
-    pprint(result_list)
-    return result_list
-
-
-
-get_missing_albums(csv_path     ='/Users/nico/Desktop/CIIC/CAPSTONE/essentia_demo/grouped_output_750_v2_clean.csv',
-                   progress_path='/Users/nico/Desktop/CIIC/CAPSTONE/essentia_demo/progress.json')
+    # Acquire the dataframe and rename it to have everything in lowercase.
+    album_df                 = pd.read_csv(csv_path)
+    album_df                 = album_df.rename(columns  = {'ALBUM'  : 'album',
+                                                           'ARTIST' : 'artist'})
+    album_df['query_artist'] = album_df['artist'].apply(process_name)
+    album_df['query_album']  = album_df['album'].apply(process_name)
+    return album_df
