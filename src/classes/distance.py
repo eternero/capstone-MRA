790f20af1fd7bc963a5556f89b0648a90a27b4bc
AngelGonzs <angel.gonzalez49@upr.edu>
2025-05-04 16:16:09 -0400

    Cleaning up distance pipeline

--- src/classes/distance.py
+++ src/classes/distance.py
@@ -3,10 +3,45 @@ import ast
 import logging
 from copy import deepcopy
 from typing import Callable
+from dataclasses import dataclass, asdict
 
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+
+
+# -------------------------------------------------------------------------------------------------
+#  Define as Global the final selected features...
+# -------------------------------------------------------------------------------------------------
+num_features          = {'electronic_effnet' : 5.0, 'instrumental_effnet' : 5.0,
+                         'acoustic_effnet'   : 3.0, 'female_effnet'    : 2.0, 'bpm' : 1.0,
+
+                         'happy_effnet'      : 3.0, 'danceable_effnet' : 3.0,
+                         'aggressive_effnet' : 3.0, 'relaxed_effnet'   : 3.0,
+                         'party_effnet'      : 3.0, 'sad_effnet'       : 3.0,
+                        }
+dim_features          = {'mfcc_mean'  : 0.01, 'discogs_effnet_embeddings' : 5.0}
+z_score_normalization = StandardScaler().fit_transform
+
+
+@dataclass
+class DistTrack:
+    """
+    A simplified Track object used to store the Distance of a Track relative to the Input Track.
+    Essentially a useful helper to keep the `DistPipeline` results nice and clean.
+
+    Attributes:
+        - track_artist   : The artist who performs the track.
+        - track_album    : The album (or EP, Compilation, Single, etc.) to which a track belongs.
+        - track_title    : The title of the track.
+        - spotify_uri    : The Spotify URI of the Track, can be used to embed as a Spotify Link.
+        - track_distance : The distance of the track relative to the input track.
+    """
+    track_artist   : str
+    track_album    : str
+    track_title    : str
+    spotify_uri    : str
+    track_distance : float
 
 
 class DistMethods:
@@ -328,249 +363,34 @@ class DistPipeline:
         return expanded_df, expanded_fts
 
 
-    def run_pipeline(self, top_n : int = 21):
-        """
-        Acquires the `top_n` recommendations for our `input_df` (input track) based on the
-        attributes passed in to the `DistPipeline` object.
+    def run_pipeline(self, top_n: int = 20) -> list[DistTrack]:
+        """Compute the top_n nearest tracks to the input track."""
+        result = []
 
-        By iterating through each track in our dataframe and comparing it to the input track with
-        the given Distance Metrics and/or Normalization Methods we gather the resulting distance
-        for each track in the dataset and then sort by the closest ones to the input track.
-
-        The input track is handled as a dataframe given that the `self.track_df` is segmented
-        by default, meaning that a single track would be represented by multiple rows of segments,
-        thus a DataFrame.
-
-        If `DistPipeline` object is decided to be pooled by passing in `pooling = True` then those
-        segments become a single row (`pd.Series`) which we then covert to a `pd.DataFrame` of a
-        single row to keep the workflow simple.
-        """
-
-        # First, make sure that our feature dictionaries are not None.
-        self.numerical_features   = {} if not self.numerical_features \
-                                       else self.numerical_features
-
-        self.dimensional_features = {} if not self.dimensional_features \
-                                       else self.dimensional_features
-
-        result_dict               = {} # Save our results here of course.
-
-        # Iterate through all the tracks in the DataFrame to compare them.
         for _, comp_track in self.mod_track_df.iterrows():
-            track_name = "-".join([comp_track['artist'], comp_track['title']])
 
-            # Iterate through each segment in the Input DataFrame
-            for _, input_segment in self.input_track_df.iterrows():
-
-                total_distance = 0
+            best_dist = float('inf')
+            for _, input_track in self.input_track_df.iterrows():
+                distance = 0.0
                 if self.numerical_features:
-                    total_distance += self.numerical_dist(input_segment, comp_track,
-                                                        self.numerical_features)
-
+                    distance += self.numerical_dist(input_track, comp_track, self.numerical_features)
                 if self.dimensional_features:
-                    total_distance += self.dimensional_dist(input_segment, comp_track,
-                                                        self.dimensional_features)
+                    distance += self.dimensional_dist(input_track, comp_track, self.dimensional_features)
 
-                result_dict[track_name] = min(total_distance, result_dict.get(track_name, float("inf")))
+                if distance < best_dist:
+                    best_dist = distance
 
+            result.append(
+                DistTrack(
+                    track_artist   = comp_track.artist,
+                    track_album    = comp_track.album,
+                    track_title    = comp_track.title,
+                    spotify_uri    = comp_track.uri,
+                    track_distance = best_dist,
+                )
+            )
 
-        # Log class attributes...
-        excluded_attributes = {"original_track_df", "mod_track_df", "input_df"}
-        logging.info(
-            "Attributes: %s",
-            ', '.join(f"{k}={v}" for k, v in vars(self).items()
-                      if k not in excluded_attributes)
-        )
-
-        # Log top similar tracks.
-        output = dict(sorted(result_dict.items(), key=lambda item: item[1]))
-        for ix, items in enumerate(output.items()):
-            if ix == 0:
-                continue
-
-            if ix >= top_n:
-                break
-            key, val = items
-            logging.info("%d. %s - %f", ix, key, val)
-        logging.info("\n"*2)
-
-        # Currently we're printing, but the top tracks can be returned, or this can be
-        # altered so that
-
-
-if __name__ == '__main__':
-    # ---------------------------------------------------------------------------------------------
-    # Define tracks to be used for testing.
-    # ---------------------------------------------------------------------------------------------
-    testing_tracks = [
-        '03 Have You Got It In You-.flac',
-        '01 - AAA Powerline.flac',
-        '02. Frou Frou - Breathe In.flac',
-        '02 Archangel.flac',
-        '08 - Sad World.flac',
-        '03 Alive in the Septic Tank.flac',
-        '15._maddington.flac',
-        '13 - Boldy James - Pots and Pans.flac',
-        '02 - Unfeeling.flac'
-    ]
-
-    # ---------------------------------------------------------------------------------------------
-    # Normalization Methods
-    # ---------------------------------------------------------------------------------------------
-    z_score_normalization = StandardScaler().fit_transform
-
-    # ---------------------------------------------------------------------------------------------
-    # HarmoF0 Pitch Features                                                                       |
-    # ---------------------------------------------------------------------------------------------
-    #   - Pitch Var is absolutely terrible. Not to be used at all.
-    #   - Pitch Salience (Not HarmoF0, Essentia)... most recommendations were bad.
-    #   - Pitch Mean and Pitch Hist were decent. Got to find the soft spot for their weights.
-    # ---------------------------------------------------------------------------------------------
-    harmof0_num_feats = {'pitch_var' : 1.0, 'pitch_mean' : 1.0}
-    harmof0_dim_feats = {'pitch_hist' : 1.0}
-
-    # ---------------------------------------------------------------------------------------------
-    # Effnet High Level / Subjective Features                                                      |
-    # ---------------------------------------------------------------------------------------------
-    #   Not very great recommendations. These either need some tuning, or to not be used at all.
-    #   The only somewhat good thing, is that at times they're able to recognize an artists style
-    #   and recommend that very same artist... but the Handpicked Set No.2 can also do this without
-    #   the plethora of inconsistencies.
-    #
-    #   The only other thing to mention is that they yield very low values, so I guess their impact
-    #   wouldn't be much... but then again, why add something that sucks?
-    #
-    #   Performs a bit better when the redundant features are removed, so that is to be considered.
-    # ---------------------------------------------------------------------------------------------
-    effnet_high_level_feats   = {'happy_effnet' : 1.0, 'danceable_effnet': 1.0,
-                                 'aggressive_effnet': 1.0, 'relaxed_effnet': 1.0,
-                                 'party_effnet': 1.0, 'sad_effnet': 1.0,
-                                }
-
-    # ---------------------------------------------------------------------------------------------
-    # Effnet Tonal Features                                                                        |
-    # ---------------------------------------------------------------------------------------------
-    #   Not sure what to say about these. Nothing to really observe. The values for the numerical
-    #   features are rather low – so I'd say that when used all together these are dominated by the
-    #   tristimulus.
-    #
-    #   Either way... the results are not good. Not horrendous, but simply not good. Maybe these
-    #   could be used with relatively low weights, since sometimes the Top 1-3 results are decent,
-    #   but overall these are not as good as others.
-    #
-    #   - Addind the `avg_dissonance` makes it a bit better surprisingly. Still not good, but
-    #     somewhat more considerable.
-    #   - The `tonal` effnet value could probably just be removed... since its mean
-    #     is literally like 94... so it just marks everything as tonal ffsk.
-    #   - Have to do more thorough testing of `bright_effnet` vs `bright_nsynth`
-    # ---------------------------------------------------------------------------------------------
-    tonal_num_features        = {'tonal_effnet' : 1.0, 'bright_effnet' : 1.0, 'avg_dissonance' : 1.0}
-    tonal_dim_features        = {'tristimulus' : 1.0}
-
-    # ---------------------------------------------------------------------------------------------
-    # Effnet Social Features                                                                       |
-    # ---------------------------------------------------------------------------------------------
-    #   Honestly pretty good for just two numerical features. Excluding the `popularity` feature
-    #   from the Spotify API is better in contrast to using it...
-    # ---------------------------------------------------------------------------------------------
-    social_effnet_features    = {'approachability_effnet':1.0, 'engagement_effnet':1.0}
-
-    # ---------------------------------------------------------------------------------------------
-    # Handpicked Features Set #1 - Starting with just a few.                                       |
-    # ---------------------------------------------------------------------------------------------
-    handpicked_num_features_1 = { 'electronic_effnet' : 1.0,
-                                 'instrumental_effnet' : 1.0, 'female_effnet' : 1.0,  'bpm' : 1.0
-                                }
-    handpicked_dim_features_1 = {'mfcc_mean' : 0.10, 'mfcc_std' : 0.05, 'tristimulus' : 1.0}    # MFCC Features have lower weight
-                                                                                                # due to their high dimensionality
-                                                                                                # more on this on my .md notes...
-    # ---------------------------------------------------------------------------------------------
-    # Handpicked Features Set #2 - A few extra features based on previous testing.                 |
-    # ---------------------------------------------------------------------------------------------
-    #       - Way better results. Can't imagine what this would be able to do with either tuning
-    #         the parameters or using contrastive learning.
-    #       - Still ways to go, and this does not yet implement nearly half of the extracted
-    #         features (not necessary to use them either, but must be tested).
-    #       - It does not implement the Pitch Histogram or tristimulus either. Both could be
-    #         implemented and would surely be of benefit.
-    # ---------------------------------------------------------------------------------------------
-    handpicked_num_features_2 = { 'electronic_effnet' : 1.0, 'instrumental_effnet' : 1.0, 'acoustic_effnet' : 0.8,
-                                  'female_effnet' : 1.0, 'bpm' : 0.65 }
-    handpicked_dim_features_2 = {'mfcc_mean' : 0.06, 'mfcc_std' : 0.15}     # Not adding tristimulus yet.
-
-    # ---------------------------------------------------------------------------------------------
-    # Handpicked Features Set #3 - Now with Discogs Effnet Embeddings as well + Weight Changes     |
-    # ---------------------------------------------------------------------------------------------
-    # The addition of the Discogs Effnet Embeddings are damn near miraculous, but they only work
-    # well with pooling. Pooling has made it so that other metrics like MFCC Mean and Std absolutely
-    # suck... but after all, they weren't damn near as good as the embeddings.
-    #
-    # These didn't add up all too well anyways, the numerical features need to be tweaked, so I'll
-    # just move on to the next round of testing.
-    # ---------------------------------------------------------------------------------------------
-    handpicked_num_features_3 = { 'electronic_effnet' : 1.2, 'instrumental_effnet' : 1.2, 'acoustic_effnet' : 0.8,
-                                  'female_effnet' : 0.5, 'bpm'     : 0.40}
-    handpicked_dim_features_3 = {'mfcc_mean'  : 0.04}
-
-    # ---------------------------------------------------------------------------------------------
-    # Handpicked Features Set #4 - MFCC Features Removed or Decreased Weights. Num Features upped  |
-    # ---------------------------------------------------------------------------------------------
-    # Satisfactory results, but some tweaks can be made. Not too sure how high the weight for the
-    # embeddings should be – sure, they do provide the best results, however smaller features also
-    # help level things out. Using optimization or triplet loss should help out figuring the values.
-    # ---------------------------------------------------------------------------------------------
-    handpicked_num_features_4 = {'electronic_effnet' : 5.0, 'instrumental_effnet' : 5.0, 'acoustic_effnet' : 3.0,
-                                 'female_effnet'     : 2.0, 'bpm'     : 1.0,
-
-                                 'happy_effnet'      : 3.0, 'danceable_effnet' : 3.0,
-                                 'aggressive_effnet' : 3.0, 'relaxed_effnet'   : 3.0,
-                                 'party_effnet'      : 3.0, 'sad_effnet'       : 3.0,
-
-                                }
-    handpicked_dim_features_4 = {'mfcc_mean'  : 0.05}
-
-    # ---------------------------------------------------------------------------------------------
-    # Handpicked Features Set #4 - MFCC Features Removed or Decreased Weights. Num Features upped  |
-    # ---------------------------------------------------------------------------------------------
-    # Satisfactory results, but some tweaks can be made. Not too sure how high the weight for the
-    # embeddings should be – sure, they do provide the best results, however smaller features also
-    # help level things out. Using optimization or triplet loss should help out figuring the values.
-    # ---------------------------------------------------------------------------------------------
-    handpicked_num_features_5 = {'electronic_effnet' : 5.0, 'instrumental_effnet' : 5.0,
-                                 'acoustic_effnet'   : 3.0, 'female_effnet'    : 2.0, 'bpm' : 1.0,
-
-                                 'happy_effnet'      : 3.0, 'danceable_effnet' : 3.0,
-                                 'aggressive_effnet' : 3.0, 'relaxed_effnet'   : 3.0,
-                                 'party_effnet'      : 3.0, 'sad_effnet'       : 3.0,
-                                 }
-    handpicked_dim_features_5 = {'mfcc_mean'  : 0.01, 'discogs_effnet_embeddings' : 5.0}
-
-    # ---------------------------------------------------------------------------------------------
-    # Running tests : Define your test parameters below for the Distance Pipeline                  |
-    # ---------------------------------------------------------------------------------------------
-    test_1  = ['electronic_effnet', 'instrumental_effnet']
-    test_2  = ['electronic_effnet', 'acoustic_effnet']
-    test_3  = ['instrumental_effnet', 'acoustic_effnet']
-
-    # With Additional Features (vocalist, bpm)
-    test_4  = ['electronic_effnet', 'bpm']
-    test_5  = ['instrumental_effnet', 'bpm']
-    test_6  = ['acoustic_effnet', 'bpm']
-
-    test_7  = ['electronic_effnet', 'female_effnet']
-    test_8  = ['instrumental_effnet', 'female_effnet']
-    test_9  = ['acoustic_effnet', 'female_effnet']
-    test_10 = ['female_effnet', 'bpm']
-
-    # Based Around Electronic and Instrumental
-    test_11 = ['electronic_effnet', 'instrumental_effnet', 'acoustic_effnet']
-    test_12 = ['electronic_effnet', 'instrumental_effnet', 'female_effnet']
-    test_13 = ['electronic_effnet', 'instrumental_effnet', 'bpm']
-
-    # Based Around Electronic and Acoustic
-    test_14 = ['electronic_effnet', 'acoustic_effnet', 'female_effnet']
-    test_15 = ['electronic_effnet', 'acoustic_effnet', 'bpm']
-    tests   = [
-                test_1, test_2, test_3, test_4, test_5, test_6, test_7, test_8, test_9, test_10,
-                test_11, test_12, test_13, test_14, test_15
-              ]
+        # NOTE : If we're given a track that is already in the dataset, then it will be it's own top
+        # result. Not sure how to deal with that yet! Metadata checking could work.
+        result.sort(key=lambda dt: dt.track_distance)
+        return [asdict(track) for track in result][:top_n + 1]
